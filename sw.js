760eed9c8b20cfa1572e33011fb8f42b1204ddb1
argea-gh <argo.armail@gmail.com>
2026-02-19 16:02:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,32 +2,38 @@ const CACHE_NAME = 'herbaprima-v1'; // PENTING: Ubah versi (v1, v2) saat update 
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './css/style.css',
-  './js/main.js',
-  './images/icons/logoherba_mob.webp',
-  './images/icons/logoherba_desk.webp'
+  './manifest.json',
+  './assets/css/style.css',
+  './assets/js/main.js',
+  './assets/js/products.js',
+  './assets/images/icons/logoherba_mob.png',
+  './assets/images/icons/logoherba_desk.png',
   './assets/images/icons/herbaprima_ss_desk.png',
   './assets/images/icons/herbaprima_ss_mob.png'
 ];
 
 // Install Event
 self.addEventListener('install', (event) => {
+  console.log('SW: Installing...');
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Membuka cache');
+      console.log('SW: Caching assets');
       return cache.addAll(ASSETS_TO_CACHE);
+    }).catch((err) => {
+      console.log('SW: Cache failed:', err);
     })
   );
 });
 
-// Activate Event (Hapus cache lama)
+// Activate Event
 self.addEventListener('activate', (event) => {
+  console.log('SW: Activating...');
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
-            console.log('Menghapus cache lama:', cache);
+            console.log('SW: Deleting old cache:', cache);
             return caches.delete(cache);
           }
         })
@@ -36,15 +42,16 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Fetch Event (Intercept request)
+// Fetch Event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Cache hit - return response
       if (response) {
         return response;
       }
       return fetch(event.request);
+    }).catch((err) => {
+      console.log('SW: Fetch failed:', err);
     })
   );
 });
